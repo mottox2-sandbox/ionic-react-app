ce590a299640ecfb66a108206e87a5f41fe942a4
mottox2 <y.mottox2@gmail.com>
2019-02-19 17:05:17 +0900

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
+import { IonApp, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
 import logo from './logo.svg';
 import './App.css';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <IonApp>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>
+              IonicReactApp
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </IonApp>
     );
   }
 }
