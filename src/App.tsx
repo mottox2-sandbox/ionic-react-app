afb212b4807b34dbb442650af2f3e035afb4d8c4
mottox2 <y.mottox2@gmail.com>
2019-02-23 02:08:14 +0900

    Fix viewport

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/react';
+import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonPage } from '@ionic/react';
 import logo from './logo.svg';
 import './App.css';
 import '@ionic/core/css/core.css';
@@ -8,25 +8,24 @@ import '@ionic/core/css/ionic.bundle.css';
 class App extends Component {
   render() {
     return (
-      // FIXME: iPhone Xのステータスバーに合わせてる。
-      <div id='app' style={{ marginTop: 44 }}>
-        <IonApp>
+      <IonApp>
+        <IonPage>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>
+                IonicReactApp
+              </IonTitle>
+            </IonToolbar>
+          </IonHeader>
           <IonContent>
-            <IonHeader>
-              <IonToolbar>
-                <IonTitle>
-                  IonicReactApp
-                </IonTitle>
-              </IonToolbar>
-            </IonHeader>
             <img src={logo} className="App-logo" alt="logo" />
             <p>
               Edit <code>src/App.tsx</code> and save to reload.
             </p>
             <IonButton href="https://reactjs.org">Learn React</IonButton>
           </IonContent>
-        </IonApp>
-      </div>
+        </IonPage>
+      </IonApp>
     );
   }
 }
